69968cec0ae64d205c98ee75f25aa86b468eb345
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-03-22 13:07:34 +0100

    Added functions for correct amount of colours and correct order, added main game loop, added logic charts in drawio, renamed drawio file

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
-var canvas;
-var ctx;
-var x = 75;
-var y = 50;
-var WIDTH = 400;
-var HEIGHT = 300;
-var dragok = false;
+const correctColour = 0
+const correctOrder = 0
 
-function rect(x,y,w,h) {
- ctx.beginPath();
- ctx.rect(x,y,w,h);
- ctx.closePath();
- ctx.fill();
+const colourPicker = (array) => {
+    const colours = ["black", "white", "red", "blue", "green", "yellow"]
+   for(i = 0; i< 4; i++){
+       j = Math.floor((Math.random()*6))
+       array[i] = colours[j]
+   }
+   return array
 }
 
-function clear() {
- ctx.clearRect(0, 0, WIDTH, HEIGHT);
+const amountCompare = (array1, array2) => {
+    let amountPoints = 0
+
+    Length = Math.min(array1.length, array2.length)
+
+    for(i = 0; i < Length; i++){
+        if(array1[i] > 0 && array2[i] > 0){
+            if(array1[i] === array2[i]){
+                amountPoints += array1[i]
+            } else{
+                amountPoints += Math.min(array1[i], array2[i])
+            }
+        }
+    }
+    return amountPoints
 }
 
-function init() {
- canvas = document.getElementById("canvas");
- ctx = canvas.getContext("2d");
- return setInterval(draw, 10);
+const orderCompare = (array1, array2) => {
+    let orderPoints = 0
+
+    Length = Math.min(array1.length, array2.length)
+
+    for(i = 0; i < Length; i++){
+        if(array1[i] === array2[i]){
+            orderPoints++
+        }
+    }
+    return orderPoints
 }
 
-function draw() {
- clear();
- ctx.fillStyle = "#FAF7F8";
- rect(0,0,WIDTH,HEIGHT);
- ctx.fillStyle = "#444444";
- rect(x - 15, y - 15, 30, 30);
-}
 
-function myMove(e){
- if (dragok){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
- }
-}
+let round = 12
 
-function myDown(e){
- if (e.pageX < x + 15 + canvas.offsetLeft && e.pageX > x - 15 +
- canvas.offsetLeft && e.pageY < y + 15 + canvas.offsetTop &&
- e.pageY > y -15 + canvas.offsetTop){
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
-  dragok = true;
-  canvas.onmousemove = myMove;
- }
-}
+while (round > 0) {
+    const generatedColour = []
+    const inputColour = []
+    
+    
+    console.log(`It's round ${round}`)
+    colourPicker(generatedColour)
+    console.log(colourArray)
 
-function myUp(){
- dragok = false;
- canvas.onmousemove = null;
-}
+    for(i = 0; i < 4; i++){
+        j = i + 1
+        inputColour[i] = prompt(`What's your pick for colour ${j}? Hint: black, white, red, blue, green or yellow `).toLowerCase()
+    }
 
-init();
-canvas.onmousedown = myDown;
-canvas.onmouseup = myUp;
+    
+
+
+    
+
+
+
+    
+    round--
+    
+}
